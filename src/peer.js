2c4d297d8e9d10a0d55fe532130201c5a1cc84a6
rdg1029 <wcle3456@gmail.com>
2021-06-19 16:43:38 +0900

    Update peer.js : Set offer & answer

--- src/peer.js
+++ src/peer.js
@@ -1,19 +1,44 @@
 class Peer {
     constructor() {
-        const iceConfig = {
+        this.iceConfig = {
             iceServers : [
                 {
                     urls: 'stun:stun1.l.google.com:19302'
                 }
             ]
         };
-        this.conn = new RTCPeerConnection(iceConfig);
-        this.conn.onicecandidate = () => {
-            console.log('New ICE candidate!');
-            console.log(JSON.stringify(this.conn.localDescription));
+        //Set offer connection
+        this.offerConn = new RTCPeerConnection(this.iceConfig);
+        this.offerConn.onicecandidate = () => {
+            console.log('New ICE candidate! (offer)');
+            console.log(JSON.stringify(this.offerConn.localDescription));
         }
-        this.dataChannel = this.conn.createDataChannel("channel");
-        this.conn.createOffer().then(offer => this.conn.setLocalDescription(offer));
+        this.dataChannel = this.offerConn.createDataChannel("channel");
+        this.dataChannel.onopen = () => console.log('open');
+        this.dataChannel.onclose = () => console.log('closed');
+
+        //Set answer connection
+        this.answerConn = new RTCPeerConnection(this.iceConfig);
+        this.answerConn.onicecandidate = () => {
+            console.log('New ICE candidate! (answer)');
+            console.log(JSON.stringify(this.answerConn.localDescription));
+        }
+        this.answerConn.ondatachannel = e => {
+            e.channel.onopen = () => console.log('open');
+            e.channel.onclose = () => console.log('closed');
+        }
+    }
+    createOffer() {
+        this.offerConn.createOffer()
+        .then(offer => this.offerConn.setLocalDescription(offer));
+    }
+    createAnswer(offer) {
+        this.answerConn.setRemoteDescription(offer).then(() => console.log('done'));
+        this.answerConn.createAnswer()
+        .then(answer => this.answerConn.setLocalDescription(answer));
+    }
+    receiveAnswer(answer) {
+        this.offerConn.setRemoteDescription(answer).then(() => console.log('done'));
     }
 }
 
