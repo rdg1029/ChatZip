55e24e322617c9001ecc77a04d627ec6eb6252ab
rdg1029 <wcle3456@gmail.com>
2021-06-23 20:50:58 +0900

    Update index.js : Add open event listener

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import {Peer} from './peer.js';
 const io = require('socket.io-client');
 const createRoom = document.getElementById('create-room');
 const enterRoom = document.getElementById('enter-room');
-//const socket = io('ADDRESS');
+const socket = io('ADDRESS');
 
 function alertNotReady() {
     window.alert('준비중입니다');
 }
 
+socket.on('open', () => {
+    //document.getElementById('status').innerHTML = "OPEN";
+    console.log('connected!');
+});
+
 createRoom.onclick = alertNotReady;
 enterRoom.onclick = alertNotReady;
 
-const peer = new Peer()
+const peer = new Peer();
