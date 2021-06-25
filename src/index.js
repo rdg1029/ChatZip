be73f0354629f9df55340343e3854e8a3012ea7b
rdg1029 <wcle3456@gmail.com>
2021-06-25 17:15:59 +0900

    Update index.js : Add join room listener

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import {Peer} from './peer.js';
 import {Room} from './room.js';
 
-const io = require('socket.io-client');
+const main = document.getElementById('main');
+const signage = document.getElementById('signage');
+const mainSignage = document.getElementById('contents-main');
 const createRoom = document.getElementById('create-room');
 const enterRoom = document.getElementById('enter-room');
-const socket = io('ADDRESS');
+
+const io = require('socket.io-client');
+const socket = io('http://localhost:3000');
 /*
 function alertNotReady() {
     window.alert('준비중입니다');
@@ -15,9 +19,17 @@ socket.on('open', () => {
     console.log('connected!');
 });
 
+socket.on('join room', room => {
+    document.body.style.backgroundColor = "#ffffff";
+    main.remove();
+    const roomName = document.createElement('h1');
+    roomName.innerHTML = 'Room : ' + room.id;
+    document.body.appendChild(roomName);
+});
+
 createRoom.onclick = () => {
     const room = new Room();
-    socket.emit('create room', room.id);
+    socket.emit('create room', room);
 }
 enterRoom.onclick = () => {
     socket.emit('req room', roomId, offer);
