e0f18dc6521e7a95c878df1629163379dbec29c6
rdg1029 <wcle3456@gmail.com>
2021-07-04 13:49:25 +0900

    Update room.js : Edit users variable

--- src/room.js
+++ src/room.js
@@ -1,7 +1,8 @@
 class Room {
     constructor(host) {
         this.id = Math.random().toString(36).substr(2,6);
-        this.host = host;
+        this.users = [];
+        this.users.push(host);
     }
     addUser(id) {
         this.users.push(id);
