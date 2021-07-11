b6534fab9a47d20ca500e587a1b8ec3a6b34f8d4
rdg1029 <wcle3456@gmail.com>
2021-07-11 15:11:00 +0900

    Update index.js : Edit room connection

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const mainSignage = document.getElementById('contents-main');
 const createRoom = document.getElementById('create-room');
 const enterRoom = document.getElementById('enter-room');
 
+let readyCount = 0;
+
 /*
 function alertNotReady() {
     window.alert('준비중입니다');
@@ -32,7 +34,18 @@ socket.on('join room', roomId => {
     document.body.appendChild(roomName);
 });
 
-socket.on('req info', targetId => {
+socket.on('user join', userId => {
+    room.addUser(userId);
+    console.log(room.users);
+});
+
+socket.on('room info', users => {
+    room.users = users;
+    console.log(room.users);
+    socket.emit('req offer', room.id, socket.id);
+});
+
+socket.on('req offer', targetId => {
     //console.log(targetId, 'request your info');
     peers[targetId] = new Peer('offer', targetId);
     peers[targetId].createOffer();
@@ -41,16 +54,28 @@ socket.on('req info', targetId => {
 socket.on('req answer', (offer, targetId) => {
     peers[targetId] = new Peer('answer', targetId);
     peers[targetId].createAnswer(offer);
-    room.addUser(targetId);
+    //room.addUser(targetId);
 });
 
 socket.on('recv answer', (answer, targetId) => {
     peers[targetId].receiveAnswer(answer);
-    room.addUser(targetId);
+    //room.addUser(targetId);
+    socket.emit('conn ready', targetId);
     console.log(room.users);
 })
 
+socket.on('conn ready', () => {
+    ++readyCount;
+    console.log('ready count :', readyCount);
+    console.log('room users :', room.users.length);
+    if(room.users.length == readyCount) {
+        console.log('req join!');
+        socket.emit('req join', room.id);
+    }
+});
+
 socket.on('room found', roomId => {
+    room.id = roomId;
     socket.emit('req info', roomId, socket.id);
 });
 
@@ -60,6 +85,10 @@ socket.on('room not found', () => {
 
 createRoom.onclick = () => {
     room.init();
+    socket.on('req info', targetId => {
+        console.log(room.users);
+        socket.emit('room info', targetId, room.users);
+    });
     socket.emit('create room', room.id);
 }
 enterRoom.onclick = () => {
