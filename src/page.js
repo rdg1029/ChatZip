31b2b7946cd9d89d8731a7837718017adf88b7c3
Caret <wcle3456@gmail.com>
2021-07-12 10:08:09 +0900

    Update page.js : Add chat page

--- src/page.js
+++ src/page.js
@@ -9,10 +9,12 @@ function setPage(page) {
     switch(page) {
         case 'main':
             document.body.innerHTML = '<div id="main"><h1>찻집</h1><div id="signage"><img src="img/signage.svg"><div id="contents-main"><p id="status">준비 중</p><button id="create-room">방 생성</button><button id="enter-room">방 입장</button></div></div></div>';
-            style.href=('./css/main.css');
+            style.href = './css/main.css';
             break;
-        case 'test':
-            document.body.innerHTML = '<h1>TEST</h1>';
+        case 'chat':
+            document.body.innerHTML = '<div id="chat"><ul id="messages"></ul><form id="form"><input id="input" /></form></div>';
+            style.href = './css/chat.css';
+            break;
     }
 }
 
