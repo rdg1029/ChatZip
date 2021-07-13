aad69468ecc3a9104e14666799281c6c3b6b3968
Caret <wcle3456@gmail.com>
2021-07-13 15:15:00 +0900

    Update room.js : Add checkIsAlone

--- src/room.js
+++ src/room.js
@@ -8,10 +8,12 @@ let room = {
     },
     addUser: function(id) {
         this.users.push(id);
+        this.checkIsAlone();
     },
     removeUser: function(id) {
         this.users.splice(this.users.indexOf(id), 1);
         this.checkHost();
+        this.checkIsAlone();
     },
     checkHost: function() {
         if(this.users[0] != socket.id) return;
@@ -23,6 +25,16 @@ let room = {
             console.log(this.users);
             socket.emit('room info', targetId, this.users);
         });
+    },
+    checkIsAlone: function() {
+        if(this.users.length == 1) {
+            socket.emit('is alone', true);
+            console.log('user alone');
+        }
+        else {
+            socket.emit('is alone', false);
+            console.log('user not alone');
+        }
     }
 }
 
