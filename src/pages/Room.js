63d6790ae025a1d029b10df410653867c875ee75
Caret <wcle3456@gmail.com>
2021-08-20 15:42:18 +0900

    Update Room.js : Init

---
+++ src/pages/Room.js
@@ -0,0 +1,11 @@
+class Room extends Page {
+    constructor(divID, css) {
+        super(divID, css);
+        this.html = '<div id="room"><canvas id="c"></canvas><div id="chat"><ul id="messages"></ul><form id="form"><input id="input" /></form></div></div>';
+    }
+    setPage() {
+        super.setPage(this.html);
+
+        this.canvas = document.getElementById('c');
+    }
+}
