1f989e3a6bb15f7a4a42aea118749d7553fb8631
Caret <wcle3456@gmail.com>
2021-08-23 16:22:47 +0900

    Update Group.js : Add isHost() & _setHost()

--- src/systems/Group.js
+++ src/systems/Group.js
@@ -1,12 +1,22 @@
 class Group {
     constructor() {
         this.id = Math.random().toString(36).substr(2,6);
-        this.users= [];
+        this.users = [];
     }
     addUser(id) {
         this.users.push(id);
+        this._setHost();
     }
     removeUser(id) {
         this.users.splice(this.users.indexOf(id), 1);
+        this._setHost();
+    }
+    isHost(id) {
+        return (this.host == id);
+    }
+
+    _setHost() {
+        if (this.host === undefined || this.host === null || this.host == users[0]) return;
+        this.host = users[0];
     }
 }
