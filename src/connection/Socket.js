4439e91e54f3b4aa518bfbd0162096039a03cd8d
Caret <wcle3456@gmail.com>
2021-08-24 15:34:06 +0900

    Update Socket.js : Init

---
+++ src/connection/Socket.js
@@ -0,0 +1,7 @@
+const io = require('socket.io-client');
+/*
+Local Test : http://localhost:3000
+Server Test : https://chatzip-signalling-server.herokuapp.com
+*/
+const socket = io('https://chatzip-signalling-server.herokuapp.com');
+export {socket};
