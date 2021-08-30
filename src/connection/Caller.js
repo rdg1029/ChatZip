87396b341a395baff4bbfe906eed2d8052cf9c38
Caret <wcle3456@gmail.com>
2021-08-30 10:46:34 +0900

    Update Caller.js : Init

---
+++ src/connection/Caller.js
@@ -0,0 +1,20 @@
+import { Peer } from './Peer';
+import { ChatChannel } from './ChatChannel';
+import { MovementChannel } from './MovementChannel';
+
+class Caller extends Peer {
+    constructor(targetId) {
+        super(targetId);
+        this.chat = new ChatChannel(this.conn, 'offer', 'chat');
+        this.movement = new MovementChannel(this.conn, 'offer', 'move');
+    }
+    createOffer() {
+        this.conn.createOffer()
+            .then(offer => this.conn.setLocalDescription(offer));
+    }
+    receiveAnswer(answer) {
+        this.conn.setRemoteDescription(answer);
+    }
+}
+
+export {Caller};
