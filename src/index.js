e7f53846c64414a18296ab16857b824eba2e8705
Caret <wcle3456@gmail.com>
2021-08-31 15:24:40 +0900

    Update index.js : Set socket listener

--- src/index.js
+++ src/index.js
@@ -40,4 +40,58 @@ function main() {
         mainPage.enterSignage.style.display = 'none';
         mainPage.mainSignage.style.display = 'block';
     };
+
+    /*Init socket listeners at main page*/
+    socket.on('open', () => {
+        mainPage.openStatus.innerHTML = "OPEN<br>(준비중)";
+        mainPage.createGroupButton.disabled = false;
+        mainPage.enterGroupButton.disabled = false;
+    });
+
+    socket.on('group found', groupId => {
+        group.id = groupId;
+
+        mainPage.typeGroupId.remove();
+        mainPage.enterButton.remove();
+        mainPage.backButton.remove();
+
+        const connMsg = document.createElement('p');
+        connMsg.innerHTML = '연결 중...';
+        mainPage.enterSignage.appendChild(connMsg);
+
+        socket.emit('req info', groupId, socket.id);
+    });
+    
+    socket.on('group not found', () => {
+        window.alert('방을 찾을 수 없습니다');
+    });
+
+    socket.on('group info', users => {
+        group.users = users;
+        socket.emit('req offer', group.id, socket.id);
+    });
+
+    socket.on('req answer', (offer, targetId) => {
+        //peers[targetId] = new Peer('answer', targetId);
+        //peers[targetId].createAnswer(offer); 
+    });
+
+    socket.on('conn ready', () => {
+        /*
+        ++readyCount;
+        if(group.users.length == readyCount) {
+            socket.emit('req join', group.id);
+        }
+        */
+    });
+
+    socket.on('join group', groupId => {
+        /*
+        removeSocketListener();
+        group.id = groupId;
+        group.addUser(socket.id);
+        setPage('room');
+        showChat('join group : ' + groupId);
+        */
+    });
 }
