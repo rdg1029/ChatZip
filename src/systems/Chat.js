eb58296ae8b9e9c9f5d499dfd9694d2796568f39
Caret <wcle3456@gmail.com>
2021-09-01 10:05:39 +0900

    Update Chat.js : Edit shotChat()

--- src/systems/Chat.js
+++ src/systems/Chat.js
@@ -2,16 +2,16 @@ class Chat {
     constructor() {
         this.form = document.getElementById('form');
         this.input = document.getElementById('input');
+        this.messages = document.getElementById('messages');
     }
     onSubmit(eventListener) {
         this.form.addEventListener('submit', eventListener);
     }
 
     showChat(msg) {
-        const messages = document.getElementById('messages');
         const message = document.createElement('li');
         message.innerHTML = msg;
-        messages.appendChild(message);
-        messages.scrollTop = messages.scrollHeight;
+        this.messages.appendChild(message);
+        this.messages.scrollTop = this.messages.scrollHeight;
     }
 }
