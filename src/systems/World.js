dc4c82814aa27729306cce31c40df9109bced8e7
rdg1029 <wcle3456@gmail.com>
2021-09-04 15:52:58 +0900

    Update World.js : Add Plane

--- src/systems/World.js
+++ src/systems/World.js
@@ -4,6 +4,7 @@ import { Loop } from './Loop';
 import { createScene } from '../components/Scene';
 import { createCamera } from '../components/Camera';
 import { createSkybox } from '../components/Skybox';
+import { createPlane } from '../components/Plane';
 
 class World {
     constructor(canvas) {
@@ -14,6 +15,11 @@ class World {
 
         const skybox = createSkybox();
         this.scene.background = skybox;
+
+        const plane = createPlane();
+        plane.rotation.x = Math.PI * -.5;
+
+        this.scene.add(plane);
     }
     start() {
         this.loop.start();
