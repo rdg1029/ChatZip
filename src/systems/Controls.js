46231b3df0de588e0210ab62fe06351b5cae8b40
Caret <wcle3456@gmail.com>
2021-09-06 10:46:14 +0900

    Update Controls.js : Add key events

--- src/systems/Controls.js
+++ src/systems/Controls.js
@@ -3,6 +3,13 @@ import { PointerLockControls } from "three/examples/jsm/controls/PointerLockCont
 class Controls extends PointerLockControls {
     constructor(camera, canvas) {
         super(camera, canvas);
+        this.key = new Map();
+        document.addEventListener('keydown', e => {
+            this.key.set(e.key, true);
+        });
+        document.addEventListener('keyup', e => {
+            this.key.set(e.key, false);
+        });
     }
 }
 
