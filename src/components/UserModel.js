d56080fbb3be4a1c62a45aa026c04a6e8edb8e5b
Caret <wcle3456@gmail.com>
2021-09-08 15:32:51 +0900

    Update UserModel.js : Add dispose()

--- src/components/UserModel.js
+++ src/components/UserModel.js
@@ -3,7 +3,12 @@ import { BoxGeometry, MeshBasicMaterial, Mesh } from 'three/build/three.min';
 function createUserModel() {
     const userGeometry = new BoxGeometry(.5, .5, .5);
     const userMaterial = new MeshBasicMaterial({color: 0x34b1eb});
-    return new Mesh(userGeometry, userMaterial);
+    const userMesh = new Mesh(userGeometry, userMaterial);
+    userMesh.dispose = () => {
+        userGeometry.dispose();
+        userMaterial.dispose();
+    }
+    return userMesh;
 }
 
 export {createUserModel};
