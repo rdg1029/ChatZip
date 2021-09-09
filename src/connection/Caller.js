a6baf5470eaa54476f9782d7c6e093746dcd177f
rdg1029 <wcle3456@gmail.com>
2021-09-09 18:05:30 +0900

    Update Caller.js : Edit datachannel variables

--- src/connection/Caller.js
+++ src/connection/Caller.js
@@ -1,12 +1,10 @@
 import { Peer } from './Peer';
-import { ChatChannel } from './ChatChannel';
-import { MovementChannel } from './MovementChannel';
 
 class Caller extends Peer {
     constructor(targetId) {
         super(targetId);
-        this.chat = new ChatChannel(this.conn, 'offer', 'chat');
-        this.movement = new MovementChannel(this.conn, 'offer', 'move');
+        this.chat = this.conn.createDataChannel('chat');
+        this.movement = this.conn.createDataChannel('move');
     }
     createOffer() {
         this.conn.createOffer()
