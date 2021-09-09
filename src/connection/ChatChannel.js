5eddb8ebdc84b97e19eb4c6e1c139facf0190d08
rdg1029 <wcle3456@gmail.com>
2021-09-09 18:15:25 +0900

    Remove data channel class

--- src/connection/ChatChannel.js
+++
@@ -1,9 +0,0 @@
-import { DataChannel } from "./DataChannel";
-
-class ChatChannel extends DataChannel {
-    constructor(peerConn, type, label) {
-        super(peerConn, type, label);
-    }
-}
-
-export {ChatChannel};
