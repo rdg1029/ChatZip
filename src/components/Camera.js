f3e193b1eaae0728793c56b41cc241f79f7f5f9b
rdg1029 <wcle3456@gmail.com>
2021-09-10 20:51:47 +0900

    Update Camera.js : Add getPositionDelta()

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -13,6 +13,18 @@ function createCamera() {
         ['y', camera.rotation.y],
         ['z', camera.rotation.z]
     ]);
+    camera.getPositionDelta = () => {
+        const delta = new Map([
+            ['x', camera.position.x - camera.prevPos.get('x')],
+            ['y', camera.position.y - camera.prevPos.get('y')],
+            ['z', camera.position.z - camera.prevPos.get('z')]
+        ]);
+        camera.prevPos.set('x', camera.position.x);
+        camera.prevPos.set('y', camera.position.y);
+        camera.prevPos.set('z', camera.position.z);
+
+        return delta;
+    }
     return camera;
 }
 
