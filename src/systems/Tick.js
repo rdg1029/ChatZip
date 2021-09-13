d822d2546c9fce625174e490ca6df2d2bfb0e69c
rdg1029 <wcle3456@gmail.com>
2021-09-13 21:35:43 +0900

    Update Tick.js : Add isStandard

--- src/systems/Tick.js
+++ src/systems/Tick.js
@@ -1,6 +1,7 @@
 class Tick {
     constructor() {
-        list = [];
+        this.list = [];
+        this.isStandard = false;
     }
     update() {
         for (let i = 0, j = this.list.length; i < j; i++) {
