b7ee22a093a3c93af8c8909bc0355f6cf4c23bd6
Caret <wcle3456@gmail.com>
2021-09-13 10:54:35 +0900

    Update Camera.js : Apply parseFloat()

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -4,19 +4,19 @@ function createCamera() {
     const camera = new PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
     camera.position.set(0, .5, 0);
     camera.prevPos = new Map([
-        ['x', camera.position.x.toFixed(3)],
-        ['y', camera.position.y.toFixed(5)],
-        ['z', camera.position.z.toFixed(3)]
+        ['x', parseFloat(camera.position.x.toFixed(3))],
+        ['y', parseFloat(camera.position.y.toFixed(5))],
+        ['z', parseFloat(camera.position.z.toFixed(3))]
     ]);
     camera.prevRot = new Map([
-        ['x', camera.rotation.x.toFixed(5)],
-        ['y', camera.rotation.y.toFixed(5)],
-        ['z', camera.rotation.z.toFixed(5)]
+        ['x', parseFloat(camera.rotation.x.toFixed(5))],
+        ['y', parseFloat(camera.rotation.y.toFixed(5))],
+        ['z', parseFloat(camera.rotation.z.toFixed(5))]
     ]);
     camera.getPositionDelta = () => {
-        const currentPosX = camera.position.x.toFixed(3);
-        const currentPosY = camera.position.y.toFixed(5);
-        const currentPosZ = camera.position.z.toFixed(3);
+        const currentPosX = parseFloat(camera.position.x.toFixed(3));
+        const currentPosY = parseFloat(camera.position.y.toFixed(5));
+        const currentPosZ = parseFloat(camera.position.z.toFixed(3));
         const delta = new Map([
             ['x', currentPosX - camera.prevPos.get('x')],
             ['y', currentPosY - camera.prevPos.get('y')],
@@ -29,9 +29,9 @@ function createCamera() {
         return delta;
     }
     camera.getRotationDelta = () => {
-        const currentRotX = camera.rotation.x.toFixed(5);
-        const currentRotY = camera.rotation.y.toFixed(5);
-        const currentRotZ = camera.rotation.z.toFixed(5);
+        const currentRotX = parseFloat(camera.rotation.x.toFixed(5));
+        const currentRotY = parseFloat(camera.rotation.y.toFixed(5));
+        const currentRotZ = parseFloat(camera.rotation.z.toFixed(5));
         const delta = new Map([
             ['x', currentRotX - camera.prevRot.get('x')],
             ['y', currentRotY - camera.prevRot.get('y')],
@@ -39,7 +39,7 @@ function createCamera() {
         ]);
         camera.prevRot.set('x', currentRotX);
         camera.prevRot.set('y', currentRotY);
-        camera.prevRot.set('z', currentRotZ)
+        camera.prevRot.set('z', currentRotZ);
 
         return delta;
     }
