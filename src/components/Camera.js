87149566d1ccd960bdd55b3d9cb9bb51b7fd5572
Caret <wcle3456@gmail.com>
2021-09-13 10:10:50 +0900

    Update Camera.js : Fixed position values

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -4,9 +4,9 @@ function createCamera() {
     const camera = new PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
     camera.position.set(0, .5, 0);
     camera.prevPos = new Map([
-        ['x', camera.position.x],
-        ['y', camera.position.y],
-        ['z', camera.position.z]
+        ['x', camera.position.x.toFixed(3)],
+        ['y', camera.position.y.toFixed(5)],
+        ['z', camera.position.z.toFixed(3)]
     ]);
     camera.prevRot = new Map([
         ['x', camera.rotation.x],
@@ -14,14 +14,17 @@ function createCamera() {
         ['z', camera.rotation.z]
     ]);
     camera.getPositionDelta = () => {
+        const currentPosX = camera.position.x.toFixed(3);
+        const currentPosY = camera.position.y.toFixed(5);
+        const currentPosZ = camera.position.z.toFixed(3);
         const delta = new Map([
-            ['x', camera.position.x - camera.prevPos.get('x')],
-            ['y', camera.position.y - camera.prevPos.get('y')],
-            ['z', camera.position.z - camera.prevPos.get('z')]
+            ['x', currentPosX - camera.prevPos.get('x')],
+            ['y', currentPosY - camera.prevPos.get('y')],
+            ['z', currentPosZ - camera.prevPos.get('z')]
         ]);
-        camera.prevPos.set('x', camera.position.x);
-        camera.prevPos.set('y', camera.position.y);
-        camera.prevPos.set('z', camera.position.z);
+        camera.prevPos.set('x', currentPosX);
+        camera.prevPos.set('y', currentPosY);
+        camera.prevPos.set('z', currentPosZ);
 
         return delta;
     }
