3358c9398b3ed62e65cedf4c4bb974efdc32c0cf
Caret <wcle3456@gmail.com>
2021-09-15 11:15:15 +0900

    Update UserModel.js : Add speed

--- src/components/UserModel.js
+++ src/components/UserModel.js
@@ -4,6 +4,16 @@ function createUserModel() {
     const userGeometry = new BoxGeometry(.5, .5, .5);
     const userMaterial = new MeshBasicMaterial({color: 0x34b1eb});
     const userMesh = new Mesh(userGeometry, userMaterial);
+
+    userMesh.speed = new Map([
+        ['posX', 0],
+        ['posY', 0],
+        ['posZ', 0],
+        ['rotX', 0],
+        ['rotY', 0],
+        ['rotZ', 0]
+    ]);
+
     userMesh.dispose = () => {
         userGeometry.dispose();
         userMaterial.dispose();
