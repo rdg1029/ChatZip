eb06f21ae7658ac455380bceb1948b42c6cb4823
rdg1029 <wcle3456@gmail.com>
2021-09-16 16:08:18 +0900

    Update UserModel.js : Change to class

--- src/components/UserModel.js
+++ src/components/UserModel.js
@@ -1,34 +1,32 @@
 import { BoxGeometry, MeshBasicMaterial, Mesh } from 'three/build/three.min';
 
-function createUserModel() {
-    const userGeometry = new BoxGeometry(.5, .5, .5);
-    const userMaterial = new MeshBasicMaterial({color: 0x34b1eb});
-    const userMesh = new Mesh(userGeometry, userMaterial);
-
-    userMesh.speed = new Map([
-        ['posX', 0],
-        ['posY', 0],
-        ['posZ', 0],
-        ['rotX', 0],
-        ['rotY', 0],
-        ['rotZ', 0]
-    ]);
-
-    userMesh.update = delta => {
+class UserModel {
+    constructor() {
+        this.userGeometry = new BoxGeometry(.5, .5, .5);
+        this.userMaterial = new MeshBasicMaterial({color: 0x34b1eb});
+        this.userMesh = new Mesh(this.userGeometry, this.userMaterial);
+        this.speed = new Map([
+            ['posX', 0],
+            ['posY', 0],
+            ['posZ', 0],
+            ['rotX', 0],
+            ['rotY', 0],
+            ['rotZ', 0]
+        ]);
+    }
+    update(delta) {
         const defaultSpeed = 10 * delta;
-        userMesh.position.x += (defaultSpeed * userMesh.speed.get('posX')).toFixed(3);
-        userMesh.position.y += (defaultSpeed * userMesh.speed.get('posY')).toFixed(5);
-        userMesh.position.z += (defaultSpeed * userMesh.speed.get('posZ')).toFixed(3);
-        userMesh.rotation.x += (defaultSpeed * userMesh.speed.get('rotX')).toFixed(5);
-        userMesh.rotation.y += (defaultSpeed * userMesh.speed.get('rotY')).toFixed(5);
-        userMesh.rotation.z += (defaultSpeed * userMesh.speed.get('rotZ')).toFixed(5);
+        this.userMesh.position.x += (defaultSpeed * this.speed.get('posX')).toFixed(3);
+        this.userMesh.position.y += (defaultSpeed * this.speed.get('posY')).toFixed(5);
+        this.userMesh.position.z += (defaultSpeed * this.speed.get('posZ')).toFixed(3);
+        this.userMesh.rotation.x += (defaultSpeed * this.speed.get('rotX')).toFixed(5);
+        this.userMesh.rotation.y += (defaultSpeed * this.speed.get('rotY')).toFixed(5);
+        this.userMesh.rotation.z += (defaultSpeed * this.speed.get('rotZ')).toFixed(5);
     }
-
-    userMesh.dispose = () => {
-        userGeometry.dispose();
-        userMaterial.dispose();
+    dispose() {
+        this.userGeometry.dispose();
+        this.userMaterial.dispose();
     }
-    return userMesh;
 }
 
-export {createUserModel};
+export {UserModel};
