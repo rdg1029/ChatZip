920e21c92ca7be6415e0a47b6d0a908b1ae45da1
rdg1029 <wcle3456@gmail.com>
2021-09-16 16:14:25 +0900

    Update UserModel.js : Edit variables name

--- src/components/UserModel.js
+++ src/components/UserModel.js
@@ -2,9 +2,9 @@ import { BoxGeometry, MeshBasicMaterial, Mesh } from 'three/build/three.min';
 
 class UserModel {
     constructor() {
-        this.userGeometry = new BoxGeometry(.5, .5, .5);
-        this.userMaterial = new MeshBasicMaterial({color: 0x34b1eb});
-        this.userMesh = new Mesh(this.userGeometry, this.userMaterial);
+        this.geometry = new BoxGeometry(.5, .5, .5);
+        this.material = new MeshBasicMaterial({color: 0x34b1eb});
+        this.mesh = new Mesh(this.geometry, this.material);
         this.speed = new Map([
             ['posX', 0],
             ['posY', 0],
@@ -16,16 +16,16 @@ class UserModel {
     }
     update(delta) {
         const defaultSpeed = 10 * delta;
-        this.userMesh.position.x += (defaultSpeed * this.speed.get('posX')).toFixed(3);
-        this.userMesh.position.y += (defaultSpeed * this.speed.get('posY')).toFixed(5);
-        this.userMesh.position.z += (defaultSpeed * this.speed.get('posZ')).toFixed(3);
-        this.userMesh.rotation.x += (defaultSpeed * this.speed.get('rotX')).toFixed(5);
-        this.userMesh.rotation.y += (defaultSpeed * this.speed.get('rotY')).toFixed(5);
-        this.userMesh.rotation.z += (defaultSpeed * this.speed.get('rotZ')).toFixed(5);
+        this.mesh.position.x += (defaultSpeed * this.speed.get('posX')).toFixed(3);
+        this.mesh.position.y += (defaultSpeed * this.speed.get('posY')).toFixed(5);
+        this.mesh.position.z += (defaultSpeed * this.speed.get('posZ')).toFixed(3);
+        this.mesh.rotation.x += (defaultSpeed * this.speed.get('rotX')).toFixed(5);
+        this.mesh.rotation.y += (defaultSpeed * this.speed.get('rotY')).toFixed(5);
+        this.mesh.rotation.z += (defaultSpeed * this.speed.get('rotZ')).toFixed(5);
     }
     dispose() {
-        this.userGeometry.dispose();
-        this.userMaterial.dispose();
+        this.geometry.dispose();
+        this.material.dispose();
     }
 }
 
