9bf7a845e19220312f06876b6d2777c728c93961
rdg1029 <wcle3456@gmail.com>
2021-09-22 14:02:33 +0900

    Update Main.js : Edit html

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,7 +3,26 @@ import {Page} from './Page.js';
 class Main extends Page {
     constructor(divID, css) {
         super(divID, css);
-        this.html = '<div id="main"><h1>찻집</h1><div id="signage"><img src="./img/signage.svg"><div class="contents" id="contents-main"><p id="open-status">준비 중</p><button class="btn" id="create-group">방 생성</button><button class="btn" id="enter-group">방 입장</button></div><div class="contents" id="contents-enter"><input id="type-group-id" type="text" /><br><button class="btn" id="enter">입장</button><br><button class="btn" id="back">뒤로 가기</button></div></div></div>';
+        this.html = `
+            <div id="main">
+                <h1>찻집</h1>
+                <div id="signage">
+                    <img src="./img/signage.svg">
+                    <div class="contents" id="contents-main">
+                        <p id="open-status">준비 중</p>
+                        <button class="btn" id="create-group">방 생성</button>
+                        <button class="btn" id="enter-group">방 입장</button>
+                    </div>
+                    <div class="contents" id="contents-enter">
+                        <input id="type-group-id" type="text" />
+                        <br>
+                        <button class="btn" id="enter">입장</button>
+                        <br>
+                        <button class="btn" id="back">뒤로 가기</button>
+                    </div>
+                </div>
+            </div>
+        `;
     }
     setPage() {
         super.setPage(this.html);
