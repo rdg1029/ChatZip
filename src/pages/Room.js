9380206170ace0b913df8def4eed065740a2e784
rdg1029 <wcle3456@gmail.com>
2021-09-23 15:15:27 +0900

    Update Room.js : Edit html

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -3,7 +3,17 @@ import {Page} from './Page.js';
 class Room extends Page {
     constructor(divID, css) {
         super(divID, css);
-        this.html = '<div id="room"><canvas id="c"></canvas><div id="chat"><ul id="messages"></ul><form id="form"><input id="input" /></form></div></div>';
+        this.html = `
+            <div id="room">
+                <canvas id="c"></canvas>
+                <div id="chat">
+                    <ul id="messages"></ul>
+                    <form id="form">
+                        <input id="input" />
+                    </form>
+                </div>
+            </div>
+        `;
     }
     setPage() {
         super.setPage(this.html);
