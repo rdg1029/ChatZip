30ad2992d045f8f28e8bc7fd653e16eadbe20852
rdg1029 <wcle3456@gmail.com>
2021-09-23 16:45:19 +0900

    Update Chat.js : remove onSubmit()

--- src/systems/Chat.js
+++ src/systems/Chat.js
@@ -1,11 +1,22 @@
+import { socket } from "../connection/Socket";
+
 class Chat {
-    constructor(room) {
+    constructor(room, peers) {
         this.messages = room.chatMessages;
         this.form = room.chatForm;
         this.input = room.chatInput;
-    }
-    onSubmit(eventListener) {
-        this.form.addEventListener('submit', eventListener);
+
+        this.form.addEventListener('submit', e => {
+            const inputValue = this.input.value;
+            e.preventDefault();
+            if(inputValue === "") return;
+            const msg = `${socket.id} : ${inputValue}`;
+            peers.forEach(peer => {
+                peer.chat.send(msg);
+            })
+            this.showChat(msg);
+            this.input.value = "";
+        });
     }
 
     showChat(msg) {
