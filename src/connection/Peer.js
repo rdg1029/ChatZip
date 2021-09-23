acfd5bcaaef2967ed997e50fda4a1ba2873742cd
rdg1029 <wcle3456@gmail.com>
2021-09-23 16:25:24 +0900

    Update Peer.js : Add setDataChannelOnMessage()

--- src/connection/Peer.js
+++ src/connection/Peer.js
@@ -1,5 +1,5 @@
 class Peer {
-    constructor(targetId) {
+    constructor(targetId, chatComponent, userModel) {
         const iceConfig = {
             iceServers : [
                 {
@@ -8,7 +8,22 @@ class Peer {
             ]
         };
         this.targetId = targetId;
+        this.chatComponent = chatComponent;
+        this.userModel = userModel;
         this.conn = new RTCPeerConnection(iceConfig);
+    }
+    setDataChannelOnMessage(peer) {
+        peer.chat.onmessage = e => this.chatComponent.showChat(e.data);
+        peer.movement.onmessage = e => {
+            const speed = new Float32Array(e.data);
+            const userSpeed = this.userModel.speed;
+            userSpeed.set('posX', speed[0]);
+            userSpeed.set('posY', speed[1]);
+            userSpeed.set('posZ', speed[2]);
+            userSpeed.set('rotX', speed[3]);
+            userSpeed.set('rotY', speed[4]);
+            userSpeed.set('rotZ', speed[5]);
+        }
     }
     close() {
         this.conn.close();
