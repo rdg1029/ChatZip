c5c11860719af3986a2b337517c88bf24bdf7060
Caret <wcle3456@gmail.com>
2021-09-24 16:35:59 +0900

    Update Camera.js : Change to class

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,49 +1,55 @@
 import { PerspectiveCamera } from "three/build/three.min";
 
+class Camera extends PerspectiveCamera {
+    constructor(fov, aspect, near, far) {
+        super(fov, aspect, near, far);
+        this.prevPos = [
+            this.position.x,
+            this.position.y,
+            this.position.z
+        ];
+        this.prevRot = [
+            this.rotation.x,
+            this.rotation.y
+        ];
+    }
+    getPositionDelta() {
+        const currentPos = [
+            this.position.x,
+            this.position.y,
+            this.position.z
+        ];
+        const delta = [
+            (currentPos[0] - prevPos[0]).toFixed(3),
+            (currentPos[1] - prevPos[1]).toFixed(5),
+            (currentPos[2] - prevPos[2]).toFixed(3)
+        ];
+        this.prevPos = [
+            this.position.x,
+            this.position.y,
+            this.position.z
+        ];
+        return delta;
+    }
+    getRotationDelta() {
+        const currentRot = [
+            this.rotation.x,
+            this.rotation.y
+        ];
+        const delta = [
+            (currentRot[0] - prevRot[0]).toFixed(5),
+            (currentRot[1] - prevRot[1]).toFixed(5)
+        ];
+        this.prevRot = [
+            this.rotation.x,
+            this.rotation.y
+        ];
+        return delta;
+    }
+}
+
 function createCamera() {
-    const camera = new PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
-    camera.position.set(0, .5, 0);
-    camera.prevPos = new Map([
-        ['x', camera.position.x],
-        ['y', camera.position.y],
-        ['z', camera.position.z]
-    ]);
-    camera.prevRot = new Map([
-        ['x', camera.rotation.x],
-        ['y', camera.rotation.y],
-        ['z', camera.rotation.z]
-    ]);
-    camera.getPositionDelta = () => {
-        const currentPosX = camera.position.x;
-        const currentPosY = camera.position.y;
-        const currentPosZ = camera.position.z;
-        const delta = new Map([
-            ['x', (currentPosX - camera.prevPos.get('x')).toFixed(3)],
-            ['y', (currentPosY - camera.prevPos.get('y')).toFixed(5)],
-            ['z', (currentPosZ - camera.prevPos.get('z')).toFixed(3)]
-        ]);
-        camera.prevPos.set('x', currentPosX);
-        camera.prevPos.set('y', currentPosY);
-        camera.prevPos.set('z', currentPosZ);
-
-        return delta;
-    }
-    camera.getRotationDelta = () => {
-        const currentRotX = camera.rotation.x;
-        const currentRotY = camera.rotation.y;
-        const currentRotZ = camera.rotation.z;
-        const delta = new Map([
-            ['x', (currentRotX - camera.prevRot.get('x')).toFixed(5)],
-            ['y', (currentRotY - camera.prevRot.get('y')).toFixed(5)],
-            ['z', (currentRotZ - camera.prevRot.get('z')).toFixed(5)]
-        ]);
-        camera.prevRot.set('x', currentRotX);
-        camera.prevRot.set('y', currentRotY);
-        camera.prevRot.set('z', currentRotZ);
-
-        return delta;
-    }
-    return camera;
+    return new Camera(60, window.innerWidth/window.innerHeight, 0.1, 100);
 }
 
 export {createCamera};
