03bc973d8eada27bfe04fdaf528a8f965fc3416b
Caret <wcle3456@gmail.com>
2021-09-24 15:46:25 +0900

    Update index.js : Edit room()

--- src/index.js
+++ src/index.js
@@ -39,58 +39,27 @@ function main() {
     });
 }
 
-function room(group, peers) {
+function room(group, offers) {
     const roomPage = new Room('room', './css/room.css');
     roomPage.setPage();
 
-    const chat = new Chat();
+    const peers = new Map();
+    const chat = new Chat(roomPage, peers);
     const world = new World(roomPage.canvas);
     const controls = new Controls(world.camera, roomPage.canvas);
-    const userModels = new Map();    
 
-    checkIsHost(peers, group, world);
+    offers.forEach((offer, id) => {
+        const userModel = createUserModel();
+        const peer = new Callee(id, chat, userModel);
+        peer.createAnswer(offer);
+        peers.set(id, peer);
+
+        world.loop.updateList.push(userModel);
+        world.scene.add(userModel.mesh);
+    });
+
+    checkIsHost(group);
     checkIsAlone(group);
-
-    peers.forEach((peer, id) => {
-        peer.chat.onmessage = e => chat.showChat(e.data);
-        peer.movement.onmessage = e => {
-            const speed = new Float32Array(e.data);
-            const userSpeed = userModels.get(id).speed;
-            userSpeed.set('posX', speed[0]);
-            userSpeed.set('posY', speed[1]);
-            userSpeed.set('posZ', speed[2]);
-            userSpeed.set('rotX', speed[3]);
-            userSpeed.set('rotY', speed[4]);
-            userSpeed.set('rotZ', speed[5]);
-        }
-        peer.tick.onmessage = e => {
-            const speedBuffer = new ArrayBuffer(24);
-            const speedArray = new Float32Array(speedBuffer);
-            const posDelta = world.camera.getPositionDelta();
-            const rotDelta = world.camera.getRotationDelta();
-            speedArray[0] = posDelta.get('x');
-            speedArray[1] = posDelta.get('y');
-            speedArray[2] = posDelta.get('z');
-            speedArray[3] = rotDelta.get('x');
-            speedArray[4] = rotDelta.get('y');
-            speedArray[5] = rotDelta.get('z');
-            peers.forEach(p => {
-                p.movement.send(speedBuffer);
-            });
-        }
-        addUserModel(world, id, userModels);
-    });
-
-    chat.onSubmit(e => {
-        e.preventDefault();
-        if(chat.input.value == "") return;
-        const msg = socket.id + " : " + chat.input.value;
-        peers.forEach(p => {
-            p.chat.send(msg);
-        })
-        chat.showChat(msg);
-        chat.input.value = "";
-    });
 
     world.loop.updateList.push(controls);
     world.start();
@@ -99,60 +68,44 @@ function room(group, peers) {
     /*Init socket listeners at room page*/
     socket.on('req offer', targetId => {
         console.log(targetId, 'requested offer');
-        const peer = new Caller(targetId);
-        peer.onIceGatheringComplete(() => socket.emit('req answer', peer.conn.localDescription, socket.id, targetId));
-        peer.chat.onmessage = e => chat.showChat(e.data);
+        const userModel = createUserModel();
+        const peer = new Caller(targetId, chat, userModel);
         peer.createOffer();
         peers.set(targetId, peer);
     });
     
     socket.on('recv answer', (answer, targetId) => {
-        peers.get(targetId).receiveAnswer(answer);
-        socket.emit('conn ready', targetId);
+        const peer = peers.get(targetId);
+        peer.receiveAnswer(answer);
+        world.loop.updateList.push(peer.userModel);
+        world.scene.add(peer.userModel.mesh);
+        // socket.emit('conn ready', targetId);
     });
 
     socket.on('user join', userId => {
         group.addUser(userId);
         chat.showChat(userId + " joined group");
-        addUserModel(world, userId, userModels);
+        // addUserModel(world, userId, userModels);
     });
     
     socket.on('user quit', userId => {
-        userModels.get(userId).dispose();
         peers.get(userId).close();
-        userModels.delete(userId);
         peers.delete(userId);
         group.removeUser(userId);
         chat.showChat(userId + " quit");
 
-        checkIsHost(peers, group, world);
+        checkIsHost(group);
         checkIsAlone(group);
     });
 }
 
-function checkIsHost(peers, group, world) {
+function checkIsHost(group) {
     if (!group.isHost(socket.id)) return;
-    world.tick.sendTick = () => {
-        const emptyBuf = new ArrayBuffer(1);
-        peers.forEach(peer => {
-            peer.tick.send(emptyBuf);
-        });
-    };
-    world.tick.isStandard = true;
-
     socket.on('req info', targetId => {
         console.log(targetId + ' requested info');
             socket.emit('group info', targetId, group.users);
     });
-
     console.log('You are host!');
-}
-
-function addUserModel(world, id, userModels) {
-    const userModel = createUserModel();
-    userModels.set(id, userModel);
-    world.loop.updateList.push(userModel);
-    world.scene.add(userModel);
 }
 
 function checkIsAlone(group) {
