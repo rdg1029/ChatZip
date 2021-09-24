4d2b14f001be4130f8a157102c3bfde067025751
Caret <wcle3456@gmail.com>
2021-09-24 16:40:09 +0900

    Update UserModel.js : Edit speed

--- src/components/UserModel.js
+++ src/components/UserModel.js
@@ -5,23 +5,15 @@ class UserModel {
         this.geometry = new BoxGeometry(.5, .5, .5);
         this.material = new MeshBasicMaterial({color: 0x34b1eb});
         this.mesh = new Mesh(this.geometry, this.material);
-        this.speed = new Map([
-            ['posX', 0],
-            ['posY', 0],
-            ['posZ', 0],
-            ['rotX', 0],
-            ['rotY', 0],
-            ['rotZ', 0]
-        ]);
+        this.speed = [0, 0, 0, 0, 0];
     }
     update(delta) {
         const defaultSpeed = 10 * delta;
-        this.mesh.position.x += (defaultSpeed * this.speed.get('posX')).toFixed(3);
-        this.mesh.position.y += (defaultSpeed * this.speed.get('posY')).toFixed(5);
-        this.mesh.position.z += (defaultSpeed * this.speed.get('posZ')).toFixed(3);
-        this.mesh.rotation.x += (defaultSpeed * this.speed.get('rotX')).toFixed(5);
-        this.mesh.rotation.y += (defaultSpeed * this.speed.get('rotY')).toFixed(5);
-        this.mesh.rotation.z += (defaultSpeed * this.speed.get('rotZ')).toFixed(5);
+        this.mesh.position.x += (defaultSpeed * this.speed[0]).toFixed(3);
+        this.mesh.position.y += (defaultSpeed * this.speed[1]).toFixed(5);
+        this.mesh.position.z += (defaultSpeed * this.speed[2]).toFixed(3);
+        this.mesh.rotation.x += (defaultSpeed * this.speed[3]).toFixed(5);
+        this.mesh.rotation.y += (defaultSpeed * this.speed[4]).toFixed(5);
     }
     dispose() {
         this.geometry.dispose();
