6abb78d60515b421059abacb085e0d00efaf9375
Caret <wcle3456@gmail.com>
2021-09-29 10:35:50 +0900

    Update Camera.js : Add getPosition() & getRotation()

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -4,40 +4,12 @@ class Camera extends PerspectiveCamera {
     constructor(fov, aspect, near, far) {
         super(fov, aspect, near, far);
         this.position.set(0, 0.5, 0);
-        this.getCurrentPos = () => {
-            return [
-                this.position.x.toFixed(3),
-                this.position.y.toFixed(5),
-                this.position.z.toFixed(3)
-            ];
-        }
-        this.getCurrentRot = () => {
-            return [
-                this.rotation.x.toFixed(5),
-                this.rotation.y.toFixed(5)
-            ];
-        }
-        this.prevPos = this.getCurrentPos();
-        this.prevRot = this.getCurrentRot();
     }
-    getPositionDelta() {
-        const currentPos = this.getCurrentPos();
-        const delta = [
-            (currentPos[0] - this.prevPos[0]).toFixed(3),
-            (currentPos[1] - this.prevPos[1]).toFixed(5),
-            (currentPos[2] - this.prevPos[2]).toFixed(3)
-        ];
-        this.prevPos = currentPos;
-        return delta;
+    getPosition() {
+        return this.position.toArray();
     }
-    getRotationDelta() {
-        const currentRot = this.getCurrentRot();
-        const delta = [
-            (currentRot[0] - this.prevRot[0]).toFixed(5),
-            (currentRot[1] - this.prevRot[1]).toFixed(5)
-        ];
-        this.prevRot = currentRot;
-        return delta;
+    getRotation() {
+        return this.rotation.toArray();
     }
 }
 
