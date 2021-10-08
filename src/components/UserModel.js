34bda9f078fb18ccb8ad37f1f21996128750dec2
Caret <wcle3456@gmail.com>
2021-10-08 13:31:13 +0900

    Update UserModel.js : Edit updateMovement()

--- src/components/UserModel.js
+++ src/components/UserModel.js
@@ -1,8 +1,8 @@
 import { Quaternion, Vector3 } from 'three';
 import { BoxGeometry, MeshBasicMaterial, Mesh } from 'three/build/three.min';
 
-const _targetPos = new Vector3();
-const _targetQt = new Quaternion();
+const _prevPos = new Vector3(), _targetPos = new Vector3();
+const _prevQt = new Quaternion(), _targetQt = new Quaternion();
 
 class UserModel {
     constructor() {
@@ -12,10 +12,10 @@ class UserModel {
     }
     updateMovement(buffer) {
         const mvArr = new Float32Array(buffer);
+        _prevPos.copy(_targetPos);
+        _prevQt.copy(_targetQt);
         _targetPos.set(mvArr[0], mvArr[1], mvArr[2]);
         _targetQt.set(mvArr[3], mvArr[4], mvArr[5], mvArr[6]);
-        this.mesh.position.lerp(_targetPos, 0.75);
-        this.mesh.quaternion.slerp(_targetQt, 0.75);
     }
     update(delta) {
         /*
