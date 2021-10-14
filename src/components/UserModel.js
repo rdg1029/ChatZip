835515ac62f1a7e08379bfe27ef3cb95b62f640d
rdg1029 <wcle3456@gmail.com>
2021-10-14 14:52:13 +0900

    Update UserModel.js : Fix users position error

--- src/components/UserModel.js
+++ src/components/UserModel.js
@@ -1,22 +1,25 @@
 import { Quaternion, Vector3 } from 'three';
 import { BoxGeometry, MeshBasicMaterial, Mesh } from 'three/build/three.min';
 
-const _prevPos = new Vector3(), _targetPos = new Vector3();
-const _prevQt = new Quaternion(), _targetQt = new Quaternion();
 let alpha = 0;
 
 class UserModel {
     constructor() {
+        this._prevPos = new Vector3();
+        this._targetPos = new Vector3();
+        this._prevQt = new Quaternion();
+        this._targetQt = new Quaternion();
+
         this.geometry = new BoxGeometry(.5, .5, .5);
         this.material = new MeshBasicMaterial({color: 0x34b1eb});
         this.mesh = new Mesh(this.geometry, this.material);
     }
     updateMovement(buffer) {
         const mvArr = new Float32Array(buffer);
-        _prevPos.copy(_targetPos);
-        _prevQt.copy(_targetQt);
-        _targetPos.set(mvArr[0], mvArr[1], mvArr[2]);
-        _targetQt.set(mvArr[3], mvArr[4], mvArr[5], mvArr[6]);
+        this._prevPos.copy(this._targetPos);
+        this._prevQt.copy(this._targetQt);
+        this._targetPos.set(mvArr[0], mvArr[1], mvArr[2]);
+        this._targetQt.set(mvArr[3], mvArr[4], mvArr[5], mvArr[6]);
         alpha = 0;
     }
     update(delta) {
@@ -27,8 +30,8 @@ class UserModel {
         else {
             alpha += delta * 10;
         }
-        this.mesh.position.lerpVectors(_prevPos, _targetPos, alpha);
-        this.mesh.quaternion.slerpQuaternions(_prevQt, _targetQt, alpha);
+        this.mesh.position.lerpVectors(this._prevPos, this._targetPos, alpha);
+        this.mesh.quaternion.slerpQuaternions(this._prevQt, this._targetQt, alpha);
     }
     dispose() {
         this.geometry.dispose();
