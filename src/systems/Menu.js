17d0591cb3ba9a427103e5b319cef0544f77d64e
rdg1029 <wcle3456@gmail.com>
2021-10-21 17:32:32 +0900

    Update Menu.js : Init

---
+++ src/systems/Menu.js
@@ -0,0 +1,13 @@
+class Menu {
+    constructor(room) {
+        this.interface = room.menu;
+        this.btnClose = room.menuBtnClose;
+        this.btnExit = room.menuBtnExit;
+    }
+    open() {
+        this.interface.style.display = 'unset';
+    }
+    close() {
+        this.interface.style.display = 'none';
+    }
+}
