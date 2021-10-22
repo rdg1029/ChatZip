88f1cace2e1486d9a9084d7913c16b4ca8610d52
rdg1029 <wcle3456@gmail.com>
2021-10-22 11:13:54 +0900

    Update Menu.js : Add isOpen

--- src/systems/Menu.js
+++ src/systems/Menu.js
@@ -3,6 +3,7 @@ class Menu {
         this.interface = room.menu;
         this.btnClose = room.menuBtnClose;
         this.btnExit = room.menuBtnExit;
+        this.isOpen = false;
     }
     open() {
         this.interface.style.display = 'unset';
