a8ae6770b23efa1424c5207c526d77e05755e57c
rdg1029 <wcle3456@gmail.com>
2021-10-22 11:26:08 +0900

    Update Menu.js : Change variable

--- src/systems/Menu.js
+++ src/systems/Menu.js
@@ -3,7 +3,7 @@ class Menu {
         this.interface = room.menu;
         this.btnClose = room.menuBtnClose;
         this.btnExit = room.menuBtnExit;
-        this.isOpen = false;
+        this.isReady = true;
     }
     open() {
         this.interface.style.display = 'unset';
