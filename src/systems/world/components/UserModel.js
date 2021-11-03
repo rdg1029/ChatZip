d72f0a23e3e289290fa6f31c9828d49f47767974
rdg1029 <wcle3456@gmail.com>
2021-11-03 17:53:25 +0900

    Update UserModel.js : Change from mesh to sprite

--- src/systems/world/components/UserModel.js
+++ src/systems/world/components/UserModel.js
@@ -1,25 +1,30 @@
-import { Quaternion, Vector3 } from 'three';
-import { BoxGeometry, MeshBasicMaterial, Mesh } from 'three/build/three.min';
+import { Vector3, TextureLoader, SpriteMaterial, Sprite } from 'three';
 
-class UserModel {
+class UserModel extends Sprite {
     constructor() {
+        const map = new TextureLoader().load('../../../../dist/img/person.png');
+        const material = new SpriteMaterial({map: map});
+        super(material);
+        
         this._prevPos = new Vector3();
         this._targetPos = new Vector3();
-        this._prevQt = new Quaternion();
-        this._targetQt = new Quaternion();
+        // this._prevQt = new Quaternion();
+        // this._targetQt = new Quaternion();
 
         this._alpha = 0;
 
-        this.geometry = new BoxGeometry(.5, .5, .5);
-        this.material = new MeshBasicMaterial({color: 0x34b1eb});
-        this.mesh = new Mesh(this.geometry, this.material);
+        // this.geometry = new BoxGeometry(.5, .5, .5);
+        // this.material = new MeshBasicMaterial({color: 0x34b1eb});
+        // this.mesh = new Mesh(this.geometry, this.material);
+        this.map = map;
+        this.material = material;
     }
     updateMovement(buffer) {
         const mvArr = new Float32Array(buffer);
         this._prevPos.copy(this._targetPos);
-        this._prevQt.copy(this._targetQt);
+        // this._prevQt.copy(this._targetQt);
         this._targetPos.set(mvArr[0], mvArr[1], mvArr[2]);
-        this._targetQt.set(mvArr[3], mvArr[4], mvArr[5], mvArr[6]);
+        // this._targetQt.set(mvArr[3], mvArr[4], mvArr[5], mvArr[6]);
         this._alpha = 0;
     }
     update(delta) {
@@ -30,11 +35,11 @@ class UserModel {
         else {
             this._alpha += delta * 10;
         }
-        this.mesh.position.lerpVectors(this._prevPos, this._targetPos, this._alpha);
-        this.mesh.quaternion.slerpQuaternions(this._prevQt, this._targetQt, this._alpha);
+        this.position.lerpVectors(this._prevPos, this._targetPos, this._alpha);
+        // this.mesh.quaternion.slerpQuaternions(this._prevQt, this._targetQt, this._alpha);
     }
     dispose() {
-        this.geometry.dispose();
+        this.map.dispose();
         this.material.dispose();
     }
 }
