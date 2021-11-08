fd952718b1e84626f75215a8e0d619af0d9be9f2
Caret <wcle3456@gmail.com>
2021-11-08 13:40:45 +0900

    Update NameLabel.js : Add makeLabelCanvas()

--- src/systems/world/components/NameLabel.js
+++ src/systems/world/components/NameLabel.js
@@ -1,1 +1,32 @@
 import { CanvasTexture, SpriteMaterial, Sprite } from 'three/build/three.min';
+
+function makeLabelCanvas(name) {
+    const baseWidth = 150;
+    const borderSize = 2;
+    const fontSize = 32;
+    const font = `${fontSize}px bold sans-serif`;
+    const context = document.createElement('canvas').getContext('2d');
+    context.font = font;
+    const nameWidth = context.measureText(name).width;
+
+    const doubleBorderSize = borderSize * 2;
+    const width = baseWidth + doubleBorderSize;
+    const height = fontSize + doubleBorderSize;
+
+    context.canvas.width = width;
+    context.canvas.height= height;
+
+    context.font = font;
+    context.textBaseLine = 'middle';
+    context.textAlign = 'center';
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
+
+    const scaleFactor = Math.min(1, baseWidth / nameWidth);
+    context.translate(width / 2, height / 2);
+    context.scale(scaleFactor, 1);
+    context.fillStyle = 'white';
+    context.fillText(name, 0, 0);
+
+    return context.canvas;
+}
