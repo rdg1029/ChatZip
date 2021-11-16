cd5ffa0bcf2c2607e169bcb4416a84e562ecc0bc
Caret <wcle3456@gmail.com>
2021-11-16 14:47:46 +0900

    Update Socket.js : Add option

--- src/systems/connection/Socket.js
+++ src/systems/connection/Socket.js
@@ -3,6 +3,6 @@ const io = require('socket.io-client');
 Local Test : http://localhost:3000
 Server Test : https://chatzip-signalling-server.herokuapp.com
 */
-const socket = io('https://chatzip-signalling-server.herokuapp.com');
+const socket = io('https://chatzip-signalling-server.herokuapp.com', { transports: ["websocket"] });
 
 export {socket};
