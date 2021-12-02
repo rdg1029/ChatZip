7e4f3769fcd2bb3240cc7521cc6506566a409082
rdg1029 <wcle3456@gmail.com>
2021-12-02 19:35:25 +0900

    Update Socket.js : Set closeOnBeforeunload to false

--- src/systems/connection/Socket.js
+++ src/systems/connection/Socket.js
@@ -3,6 +3,9 @@ const io = require('socket.io-client');
 Local Test : http://localhost:3000
 Server Test : https://chatzip-signalling-server.herokuapp.com
 */
-const socket = io('https://chatzip-signalling-server.herokuapp.com', { transports: ["websocket"] });
+const socket = io('https://chatzip-signalling-server.herokuapp.com', { 
+    transports: ["websocket"],
+    closeOnBeforeunload: false
+});
 
 export {socket};
