5a629c31ae64ae48216b36a375ae0c3dd3260357
rdg1029 <wcle3456@gmail.com>
2022-03-21 21:39:20 +0900

    Update User.js : Add update()

--- src/systems/User.js
+++ src/systems/User.js
@@ -6,13 +6,20 @@ const user = {
     state: {
         pos: [0, 0, 0],
         onGround: false,
+        gravity: 1,
         gravAccel: 0,
-        jumpHeight: 0.2,
+        jumpHeight: 1,
     },
     colllision: {
         width: 4,
         height: 14,
         depth: 4,
+    },
+    update: function(delta) {
+        this.state.gravAccel -= this.state.gravity * delta;
+        if (this.state.pos[1] > 0) return;
+        this.state.pos[1] = 0;
+        this.state.onGround = true;
     },
 };
 
