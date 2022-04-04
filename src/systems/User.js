ed75c14f143413c2d9568b3ba4b50bba32c8f41c
Caret <wcle3456@gmail.com>
2022-04-04 13:51:27 +0900

    Update User.js : Remove update()

--- src/systems/User.js
+++ src/systems/User.js
@@ -5,7 +5,7 @@ const user = {
     },
     state: {
         pos: [0, 0, 0],
-        dir: [0, 0, 0],
+        velocity: [0, 0, 0],
         onGround: true,
         gravity: 3.2,
         gravAccel: 0,
@@ -15,14 +15,6 @@ const user = {
         width: 4,
         height: 14,
         depth: 4,
-    },
-    update: function(delta) {
-        if (this.state.onGround) {
-            this.state.gravAccel = 0;
-            return;
-        }
-        this.state.gravAccel -= this.state.gravity * delta;
-        this.state.dir[1] += this.state.gravAccel;
     },
 };
 
