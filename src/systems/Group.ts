b29cf5ab59711efd93e576d798fd47068674b9a8
rdg1029 <wcle3456@gmail.com>
2022-04-08 22:21:55 +0900

    Update Group : Apply typescript

--- src/systems/Group.ts
+++ src/systems/Group.ts
@@ -1,35 +1,39 @@
 class Group {
+    public id: string;
+    public number: number;
+    public users: string[];
+    public host: string;
+
     constructor() {
         this.number = 0;
         this.users = [];
     }
-    createNewId() {
+    private setHost() {
+        if (this.number != 1 && this.host == this.users[0]) return;
+        this.host = this.users[0];
+    }
+    public createNewId() {
         this.id = Math.random().toString(36).substr(2,6);
     }
-    addUser(id) {
+    public addUser(id: string) {
         this.number = this.users.push(id);
-        this._setHost();
+        this.setHost();
     }
-    removeUser(id) {
+    public removeUser(id: string) {
         if (this.number == 0) {
             console.error('No users in this group!');
             return;
         }
         this.users.splice(this.users.indexOf(id), 1);
         if (--this.number == 0) return;
-        this._setHost();
+        this.setHost();
     }
-    isHost(id) {
+    public isHost(id: string) {
         if (this.host === undefined || this.host === null) {
             console.error('Host is undefined or null');
             return;
         }
         return (this.host == id);
-    }
-
-    _setHost() {
-        if (this.number != 1 && this.host == this.users[0]) return;
-        this.host = this.users[0];
     }
 }
 
