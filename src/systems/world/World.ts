4930d3d61002fdd38e32ee843287ded437ac1daf
rdg1029 <wcle3456@gmail.com>
2022-04-09 12:22:45 +0900

    Update World : Apply typescript

--- src/systems/world/World.ts
+++ src/systems/world/World.ts
@@ -1,8 +1,10 @@
+import { WebGLRenderer, Scene } from 'three';
+
 import { createRenderer } from './Renderer';
 import { Loop } from './Loop';
 
 import { createScene } from './components/Scene';
-import { createCamera } from './components/Camera';
+import { createCamera, Camera } from './components/Camera';
 import { createSkybox } from './components/Skybox';
 import { createLight } from './components/Light';
 // import { createPlane } from './components/Plane';
@@ -10,7 +12,13 @@ import { UserModel } from './components/user_model/UserModel';
 import { VoxelMap } from './components/VoxelMap';
 
 class World {
-    constructor(canvas) {
+    public renderer: WebGLRenderer;
+    public scene: Scene;
+    public camera: Camera;
+    public loop: Loop;
+    public map: VoxelMap;
+
+    constructor(canvas: HTMLCanvasElement) {
         this.renderer = createRenderer(canvas);
         this.scene = createScene();
         this.camera = createCamera();
@@ -31,11 +39,10 @@ class World {
         const light = createLight();
         this.scene.add(light);
     }
-    createUserModel(name) {
+    createUserModel(name: string) {
         return new UserModel(name);
     }
     start() {
-        this.map.load();
         this.loop.start();
     }
     stop() {
