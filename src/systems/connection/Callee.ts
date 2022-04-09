5e2c8dbfc194ef9246d4dd443a74cd2dd97541b7
rdg1029 <wcle3456@gmail.com>
2022-04-09 09:44:02 +0900

    Update Callee : Apply typescript

--- src/systems/connection/Callee.ts
+++ src/systems/connection/Callee.ts
@@ -1,19 +1,22 @@
+import { UserData } from '../User';
+import { Chat } from '../Chat';
+import { UserModel } from '../world/components/user_model/UserModel';
 import { socket } from './Socket';
 import { user } from '../User';
 import { Peer } from './Peer';
 
 class Callee extends Peer {
-    constructor(targetUserData, chatComponent, userModel) {
+    constructor(targetUserData: UserData, chatComponent: Chat, userModel: UserModel) {
         super(targetUserData, chatComponent, userModel);
 
         this.conn.onicegatheringstatechange = e => {
             console.log('ice gathering...');
-            if (e.target.iceGatheringState !== 'complete') return;
+            if (this.conn.iceGatheringState !== 'complete') return;
             console.log('ice gathering complete!');
             socket.emit('recv answer', this.conn.localDescription, user.data, targetUserData.id);
         }
     }
-    createAnswer(offer) {
+    createAnswer(offer: RTCSessionDescriptionInit) {
         this.conn.setRemoteDescription(offer);
         this.conn.createAnswer()
             .then(answer => this.conn.setLocalDescription(answer));
