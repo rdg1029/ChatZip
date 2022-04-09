402abff3271c3ec23b52076a2520d4b65464a4f6
rdg1029 <wcle3456@gmail.com>
2022-04-09 11:41:00 +0900

    Remove Plane.js

--- src/systems/world/components/Plane.js
+++
@@ -1,9 +0,0 @@
-import { PlaneGeometry, MeshBasicMaterial, Mesh } from "three";
-
-function createPlane() {
-    const planeGeometry = new PlaneGeometry(10, 10);
-    const planeMaterial = new MeshBasicMaterial({color: 0xffffff});
-    return new Mesh(planeGeometry, planeMaterial);
-}
-
-export {createPlane};
