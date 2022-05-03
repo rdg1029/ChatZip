60587d6500baaa14652ec6ff038056810c1c8586
Caret <wcle3456@gmail.com>
2022-05-03 13:28:30 +0900

    Update Light : Set matrix auto update to false

--- src/systems/world/components/Light.ts
+++ src/systems/world/components/Light.ts
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 function createLight() {
     const light = new THREE.HemisphereLight(0xffffff, 0xaaaaaa, 1);
     light.position.set(0.5, 1, 0.75);
+    light.matrixAutoUpdate = false;
     return light;
 }
 
