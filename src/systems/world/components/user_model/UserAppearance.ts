054c9e59bfa43de75617f79fb5fd79e216719513
rdg1029 <wcle3456@gmail.com>
2022-05-07 10:51:29 +0900

    Update UserAppearance : Edit constructor

--- src/systems/world/components/user_model/UserAppearance.ts
+++ src/systems/world/components/user_model/UserAppearance.ts
@@ -10,15 +10,6 @@ class UserAppearance extends Mesh {
         const material = new MeshBasicMaterial({map: map, alphaTest: 0.5});
         super(plane, material);
         this.map = map;
-    }
-    updateRotationPoint() {
-        const geometry = this.geometry;
-        const center = new Vector3();
-
-        geometry.computeBoundingBox();
-        geometry.boundingBox.getCenter(center);
-        geometry.center();
-        this.position.copy(center);
         this.position.y += 8;
     }
     dispose() {
